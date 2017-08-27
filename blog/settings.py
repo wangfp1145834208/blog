8f421f52a87d83205e247bd2fde32f2a7285dc8c
wangfp <wangfp_nk@163.com>
2017-08-28 00:19:22 +0800

    comments

--- blog/settings.py
+++ blog/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blogs',
+    'comments',
 ]
 
 MIDDLEWARE = [
@@ -116,7 +117,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False 
 
 
 # Static files (CSS, JavaScript, Images)
--- blogs/views.py
+++ blogs/views.py
@@ -1,13 +1,31 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+import markdown
 
-from .models import Post
+from .models import Post, Category
 
 def index(request):
-    post_list = Post.objects.all()
+    post_list = Post.objects.all().order_by('-created_time')
     context = {'post_list': post_list}
     return render(request, 'blogs/index.html', context=context)
 
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
+    post.body = markdown.markdown(post.body,
+            extensions=[
+                'markdown.extensions.extra',
+                'markdown.extensions.codehilite',
+                'markdown.extensions.toc',
+                ])
     return render(request, 'blogs/detail.html', context={'post': post})
+
+def archives(request, year, month):
+    post_list = Post.objects.filter(created_time__year=year,
+                                    created_time__month=month
+                                    ).order_by('-created_time')
+    return render(request, 'blogs/index.html', context={'post_list': post_list})
+
+def category(request, pk):
+    cate = get_object_or_404(Category, pk=pk)
+    post_list = Post.objects.filter(category=cate).order_by('-created_time')
+    return render(request, 'blogs/index.html', context={'post_list': post_list})
